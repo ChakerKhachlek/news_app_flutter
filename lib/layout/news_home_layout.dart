e18aa6691a2335a1cfdc23b10cb227b17d1cdd91
ChakerKhachlek <chaker-@hotmail.com>
2021-08-09 18:23:31 +0100

    "first commit"

---
+++ lib/layout/news_home_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NewsHomeLayout extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
